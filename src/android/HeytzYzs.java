6b7533fe42718174cca84a4443eb2e6364bcc5ee
陈东东 <chendongdong@chendongdongdeMacBook-Pro.local>
2017-06-21 17:51:38 +0800

    修改plugin.xml配置

--- src/android/HeytzYzs.java
+++ src/android/HeytzYzs.java
@@ -2,9 +2,11 @@ package com.heytz.yzs;
 
 import android.content.Context;
 import android.util.Log;
+
 import com.unisound.client.SpeechConstants;
 import com.unisound.client.SpeechUnderstander;
 import com.unisound.client.SpeechUnderstanderListener;
+
 import org.apache.cordova.*;
 
 import org.json.JSONArray;
@@ -88,17 +90,17 @@ public class HeytzYzs extends CordovaPlugin {
                     break;
                 case SpeechConstants.ASR_EVENT_RECORDING_START:
                     //录音设备打开，开始识别，用户可以开始说话
-                    statue =  AsrStatus.recording;
+                    statue = AsrStatus.recording;
                     break;
                 case SpeechConstants.ASR_EVENT_RECORDING_STOP:
                     // 停止录音，请等待识别结果回调
                     log_v("onRecordingStop");
-                    statue =  AsrStatus.recognizing;
+                    statue = AsrStatus.recognizing;
                     break;
                 case SpeechConstants.ASR_EVENT_USERDATA_UPLOADED:
                     log_v("onUploadUserData");
                 case SpeechConstants.ASR_EVENT_NET_END:
-                    statue =  AsrStatus.idle;
+                    statue = AsrStatus.idle;
                     break;
             }
             String format = "cordova.plugins.HeytzYzs.speechUnderstanderEvent(%d,%s);";
@@ -119,11 +121,81 @@ public class HeytzYzs extends CordovaPlugin {
      */
     private void initRecognizer() {
         // 创建识别对象，appKey通过 http://dev.hivoice.cn/ 网站申请
-        mUnderstander = new SpeechUnderstander(cordova.getActivity().getApplicationContext(), APP_ID, APP_SECRET);
+        mUnderstander = new SpeechUnderstander(this.cordova.getActivity().getApplication().getBaseContext(), APP_ID, APP_SECRET);
         //设置无语义结果
         mUnderstander.setOption(SpeechConstants.NLU_ENABLE, false);
-        mUnderstander.setListener(speechUnderstanderListener);
-        mUnderstander.init("");
+        mUnderstander.setListener(new SpeechUnderstanderListener() {
+
+            @Override
+            public void onResult(int type, String jsonResult) {
+                switch (type) {
+                    case SpeechConstants.ASR_RESULT_NET:
+                        // 在线识别结果，通常onResult接口多次返回结果，保留识别结果组成完整的识别内容。
+                        log_v("onRecognizerResult");
+                        // 通常onResult接口多次返回结果，保留识别结果组成完整的识别内容。
+                        if (jsonResult.contains("net_asr")) {
+                            try {
+                                JSONObject json = new JSONObject(jsonResult);
+                                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, json);
+                                pluginResult.setKeepCallback(true);
+                                sendCallback(pluginResult);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                String format = "cordova.plugins.HeytzYzs.speechUnderstanderResult(%d,%s);";
+                final String js = String.format(format, type, jsonResult);
+                loadUrl(js);
+            }
+
+            @Override
+            public void onEvent(int type, int timeMs) {
+                switch (type) {
+                    case SpeechConstants.ASR_EVENT_VAD_TIMEOUT:
+                        // 说话音量实时返回
+                        log_v("onVADTimeout");
+                        // 收到用户停止说话事件，停止录音
+                        stopRecord();
+                        break;
+                    case SpeechConstants.ASR_EVENT_VOLUMECHANGE:
+                        // 说话音量实时返回
+                        break;
+                    case SpeechConstants.ASR_EVENT_SPEECH_DETECTED:
+                        //用户开始说话
+                        log_v("onSpeakStart");
+                        break;
+                    case SpeechConstants.ASR_EVENT_RECORDING_START:
+                        //录音设备打开，开始识别，用户可以开始说话
+                        statue = AsrStatus.recording;
+                        break;
+                    case SpeechConstants.ASR_EVENT_RECORDING_STOP:
+                        // 停止录音，请等待识别结果回调
+                        log_v("onRecordingStop");
+                        statue = AsrStatus.recognizing;
+                        break;
+                    case SpeechConstants.ASR_EVENT_USERDATA_UPLOADED:
+                        log_v("onUploadUserData");
+                    case SpeechConstants.ASR_EVENT_NET_END:
+                        statue = AsrStatus.idle;
+                        break;
+                }
+                String format = "cordova.plugins.HeytzYzs.speechUnderstanderEvent(%d,%s);";
+                final String js = String.format(format, type, timeMs);
+                loadUrl(js);
+            }
+
+            @Override
+            public void onError(int type, final String errorMSG) {
+                String format = "cordova.plugins.HeytzYzs.speechUnderstanderError(%d,%s);";
+                final String js = String.format(format, type, errorMSG);
+                loadUrl(js);
+            }
+        });
+        mUnderstander.init(null);
     }
 
     @Override
@@ -137,10 +209,11 @@ public class HeytzYzs extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals(INIT)) {
+            initRecognizer();
             callbackContext.success();
             return true;
         } else if (action.equals(START_RECORD)) {
-            if (statue== AsrStatus.idle) {
+            if (statue == AsrStatus.idle) {
                 this.startRecord(callbackContext);
             } else {
                 callbackContext.error("running");
@@ -148,7 +221,7 @@ public class HeytzYzs extends CordovaPlugin {
             return true;
         } else if (action.equals(STOP_RECORD)) {
             if (stopRecord()) {
-                statue =  AsrStatus.idle;
+                statue = AsrStatus.idle;
                 callbackContext.success();
             } else {
                 callbackContext.error("not start");
@@ -179,10 +252,10 @@ public class HeytzYzs extends CordovaPlugin {
      * 停止录音
      */
     boolean stopRecord() {
-        if (statue ==  AsrStatus.recording) {
+        if (statue == AsrStatus.recording) {
             mUnderstander.stop();
             return true;
-        } else if (statue ==  AsrStatus.recognizing) {
+        } else if (statue == AsrStatus.recognizing) {
             mUnderstander.cancel();
             return true;
         } else {
